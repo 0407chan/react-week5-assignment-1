2c17fe6a8172b6f7f7a7a15a273656691d5831ad
이찬호 <0407chan@naver.com>
2022-04-20 23:57:58 +0900

    Updates

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -2,31 +2,57 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import regions from '../fixtures/regions';
+import categories from '../fixtures/categories';
+import restaurants from '../fixtures/restaurants';
+
 import App from './App';
+import { loadRegions } from '../redux/actions';
 
 jest.mock('react-redux');
 jest.mock('../services/api');
 
-test('App', () => {
+describe('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [{
-      id: 1,
-      name: '두향',
-    },
-    {
-      id: 2,
-      name: '맥도날드',
-    },
-    ],
+    regions,
+    categories,
+    restaurants,
   }));
 
-  const { queryByText } = render((
-    <App />
-  ));
+  function renderApp() {
+    return render(<App />);
+  }
 
-  expect(queryByText(/두향/)).not.toBeNull();
-  expect(queryByText(/맥도날드/)).not.toBeNull();
+  it('fetchs regions, categories, restaurants', () => {
+    renderApp();
+
+    console.log('뭐나와', dispatch);
+
+    expect(dispatch).toBeCalledTimes(3);
+    expect(dispatch).toBeCalledWith(loadRegions());
+  });
+
+  it('renders regions', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/서울/)).not.toBeNull();
+    expect(queryByText(/대전/)).not.toBeNull();
+  });
+
+  it('renders categories', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/한식/)).not.toBeNull();
+    expect(queryByText(/일식/)).not.toBeNull();
+  });
+
+  it('renders restautants', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/두향/)).not.toBeNull();
+    expect(queryByText(/맥도날드/)).not.toBeNull();
+  });
 });
--- src/components/Buttons.test.jsx
+++ src/components/Buttons.test.jsx
@@ -2,21 +2,16 @@ import { fireEvent, render } from '@testing-library/react';
 import given from 'given2';
 import 'given2/setup';
 
-import { useDispatch } from 'react-redux';
+import regions from '../fixtures/regions';
+import categories from '../fixtures/categories';
 
 import Buttons from './Buttons';
 
 describe('Buttons', () => {
   given('value', () => 'name');
-  given('buttonList', () => [{
-    id: 1,
-    name: '서울',
-  }]);
+  given('buttonList', () => regions);
   given('currentButtonInfo', () => '서울');
   given('emptyMessage', () => '정보가 없어요!');
-
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
 
   const handleButtonClick = jest.fn();
 
@@ -40,22 +35,10 @@ describe('Buttons', () => {
     given('value', () => 'name');
     given('currentButtonInfo', () => '');
     given('emptyMessage', () => '지역이 없어요!');
-    given('buttonList', () => [{
-      id: 1,
-      name: '서울',
-    }, {
-      id: 2,
-      name: '대전',
-    }]);
+    given('buttonList', () => regions);
 
     context('with buttonList', () => {
-      given('buttonList', () => [{
-        id: 1,
-        name: '서울',
-      }, {
-        id: 2,
-        name: '대전',
-      }]);
+      given('buttonList', () => regions);
 
       it('renders regions', () => {
         const { container } = renderButtons();
@@ -104,17 +87,32 @@ describe('Buttons', () => {
         expect(container).toHaveTextContent(/정보가 없어요!/);
       });
     });
+
+    context('with currentButtonInfo', () => {
+      given('buttonList', () => regions);
+      given('currentButtonInfo', () => '서울');
+
+      it('renders "(V)" when region name is match to currentButtonInfo', () => {
+        const { queryByText } = renderButtons();
+
+        expect(queryByText(/(V)/)).not.toBe(null);
+      });
+    });
+    context('without currentButtonInfo', () => {
+      given('buttonList', () => regions);
+      given('currentButtonInfo', () => undefined);
+
+      it('doesn\'t renders "(V)"', () => {
+        const { queryByText } = renderButtons();
+
+        expect(queryByText(/(V)/)).toBe(null);
+      });
+    });
   });
 
   describe('Buttons for categories', () => {
     given('value', () => 'id');
-    given('buttonList', () => [{
-      id: 1,
-      name: '한식',
-    }, {
-      id: 2,
-      name: '중식',
-    }]);
+    given('buttonList', () => categories);
     given('currentButtonInfo', () => -1);
     given('emptyMessage', () => '카테고리가 없어요!');
 
@@ -123,13 +121,13 @@ describe('Buttons', () => {
         const { container } = renderButtons();
 
         expect(container).toHaveTextContent(/한식/);
-        expect(container).toHaveTextContent(/중식/);
+        expect(container).toHaveTextContent(/일식/);
       });
 
       it('updates categoryId when clicks the category button', () => {
         const { getByText } = renderButtons();
 
-        fireEvent.click(getByText('중식'));
+        fireEvent.click(getByText('일식'));
 
         expect(handleButtonClick).toBeCalledWith({ value: 2 });
       });
